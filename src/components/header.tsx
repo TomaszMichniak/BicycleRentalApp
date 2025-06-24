d919bd95baf899d24c32b774d42208719501789b
Tomasz Michniak <Tomasz.michniak01@gmail.com>
2025-06-24 14:17:06 +0200

    header image src change

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return(
-        <header  className="bg-[url('./src/assets/DSC_11_640.jpg')] md:bg-[url('./src/assets/11.jpg')] bg-cover bg-center h-96 md:h-[700px] lg:h-[800px] relative">
+        <header  className="bg-[url('./assets/DSC_11_640.jpg')] md:bg-[url('./assets/11.jpg')] bg-cover bg-center h-96 md:h-[700px] lg:h-[800px] relative">
         <div  className="w-full h-full bg-black/10 absolute  " ></div>
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center">
             <h1 className="text-2xl md:text-5xl mb-3">Wypożyczalnia rowerów elektrycznych</h1>
